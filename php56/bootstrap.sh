e2016a6d364984d0008fd429d3f9cc45e0744bac
Pavel Teplyuk <xakerpaul@gmail.com>
2017-12-05 23:23:49 +0700

    small fixes

--- php56/bootstrap.sh
+++ php56/bootstrap.sh
@@ -6,7 +6,7 @@
 # Do not modify this file as it is tracked by source control.
 
 echo "Starting apache web server..."
-sudo /usr/local/bin/apache2-foreground
+/usr/local/bin/apache2-foreground
 
 # A note on performance: It is typically better to keep this script
 # light with no process-intensive operations as it will be run on every
